af75a91c542aa23f7bc6283aff5ece9f9d101146
fepezoabarca <fepezoabarca@users.noreply.github.com>
2024-04-03 21:46:57 -0300

    Add files via upload
    Primer carga

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $("#btn-contactar").click(function () {
+        alert("El correo fue enviado correctamente.");
+    });
+});
+
+// Initialize Bootstrap tooltips
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialize Bootstrap tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
